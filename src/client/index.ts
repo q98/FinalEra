87b5094f6bf581a758b1f49142e5823a2716b2d3
root <root@server1.example.com>
2020-10-10 02:55:12 -0500

    server started for tag, might change some stoof

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,7 +1,29 @@
 import { ArcRotateCamera, Engine, HemisphericLight, MeshBuilder, Scene, Vector3 } from "babylonjs"
 import { AdvancedDynamicTexture } from "babylonjs-gui"
-
+import io from 'socket.io-client'
 import { GUI, xAddPos, yAddPos } from "./core/GUI"
+
+    let socket = io('http://104.237.1.23:3000')
+
+    //socket.connect('http://104.237.1.23:3000'); 
+
+    // Add a connect listener
+    socket.on('connect',function() {
+      console.log('Client has connected to the server!');
+    });
+    // Add a connect listener
+    socket.on('message',function(data) {
+      console.log('Received a message from the server!',data);
+    });
+    // Add a disconnect listener
+    socket.on('disconnect',function() {
+      console.log('The client has disconnected!');
+    });
+
+    // Sends a message to the server via sockets
+    function sendMessageToServer(message) {
+      socket.send(message);
+    };
 
 const view = document.getElementById("view") as HTMLCanvasElement
 const engine = new Engine(view, true)
--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -10,5 +10,6 @@ module.exports = merge(common, {
         contentBase: path.resolve(__dirname, "./dist"),
         compress: true,
         port: 9000,
+        host: '0.0.0.0'
     },
 })
